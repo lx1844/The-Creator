3e0b66a7101849f4d2bd4610dcdce58dd95f8c8e
lxchipalvarez184 <lexvalencia0001@gmail.com>
2025-04-29 17:53:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,68 @@
+# PLAYER DATA
+PLAYER = {
+    "LVL": 1,
+    "EXP": 0,
+    "HP ": 100,
+    "ATK": 2,
+    "BLK": 0,
+    "STL": 0,
+    "CRI": 0,
+    "MAG": 0,
+    "GEM": 100
+}
+
+CREATOR = {
+    "LVL": 1,
+    "EXP": 0,
+    "HP ": 150,
+    "ATK": 5,
+    "BLK": 5,
+    "STL": 5,
+    "CRI": 5,
+    "MAG": 0,
+}
+
+# MAX VALUES
+P_MAX = {
+    "HP": 999,
+    "ATK": 999,
+    "BLK": 100,
+    "STL": 100,
+    "CRIT": 100,
+    "MAG": 100
+}
+
+# CHECKERS
+CHECK = {
+    "BLK_ON": 0,
+    "STL_ON": 0,
+    "CRIT_ON": 0,
+    "MAG_ON": 0
+}
+
+# SHOP
+SHOP = {
+    "BLK": 999,
+    "CRIT": 3999,
+    "STL": 4999,
+    "MAG": 9999
+}
+
+# FONTS AND COLORS
+CEND     = "\033[0m"
+BOLD      = "\033[1m"
+ULINE = "\033[4m"
+RED       = "\033[31m"
+GREEN     = "\033[32m"
+YELLOW     = "\033[33m"
+BLUE     = "\033[34m"
+PURPLE     = "\033[35m"
+AQUA     = "\033[36m"
+GREY     = "\033[37m"
+
+
+TEST_NAME = 'THIS IS A TEST ...'
+
+# DEBUG
+# print(GREEN + UNDERLINE + "Hi")
+
